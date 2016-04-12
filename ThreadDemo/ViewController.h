6c4004c758df1b06f46dfc89bf497e0f0aa7cd60
caohuoxia <791245051@qq.com>
2016-04-12 16:23:48 +0800

    first commit simple use about nsthread synchronized runloop

---
+++ ThreadDemo/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  ThreadDemo
+//
+//  Created by admin on 16/4/12.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+//情景：某演唱会门票发售，在广州和北京均开设窗口进行销售
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ThreadDemo/ViewController.m
@@ -0,0 +1,90 @@
+//
+//  ViewController.m
+//  ThreadDemo
+//
+//  Created by admin on 16/4/12.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+{
+    NSInteger ticketCount;
+}
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    ticketCount = 50;
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(threadExitNotice) name:NSThreadWillExitNotification object:nil];
+    NSThread * window1 = [[NSThread alloc]initWithTarget:self selector:@selector(thread1) object:nil];
+    [window1 start];
+    NSThread * window2 = [[NSThread alloc]initWithTarget:self selector:@selector(thread2) object:nil];
+    [window2 start];
+    
+    [self performSelector:@selector(saleTicket) onThread:window1 withObject:nil waitUntilDone:NO];
+    [self performSelector:@selector(saleTicket) onThread:window2 withObject:nil waitUntilDone:NO];
+//    //新建两个子线程（代表两个窗口同时销售门票）
+//    NSThread *thread_beijing = [[NSThread alloc] initWithTarget:self selector:@selector(saleTicket) object:nil];
+//    thread_beijing.name = @"北京售票窗口";
+//    [thread_beijing start];
+//    NSThread *thread_guangzhou = [[NSThread alloc] initWithTarget:self selector:@selector(saleTicket) object:nil];
+//    thread_guangzhou.name = @"广州售票窗口";
+//    [thread_guangzhou start];
+}
+
+- (void)thread1 {
+    [NSThread currentThread].name = @"北京售票窗口";
+    NSRunLoop * runLoop1 = [NSRunLoop currentRunLoop];
+    [runLoop1 runUntilDate:[NSDate date]]; //一直运行
+}
+- (void)thread2 {
+    [NSThread currentThread].name = @"广州售票窗口";
+    NSRunLoop * runLoop2 = [NSRunLoop currentRunLoop];
+    [runLoop2 runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:10.0]]; //自定义运行时间
+}
+
+- (void)threadExitNotice{
+    
+}
+
+//线程启动后，执行saleTicket，执行完毕后就会退出，为了模拟持续售票的过程，我们需要给它加一个循环
+- (void)saleTicket {
+    while (1) {
+        @synchronized(self) {
+            //如果还有票，继续售卖
+            if (ticketCount > 0) {
+                ticketCount --;
+                NSLog(@"%@", [NSString stringWithFormat:@"剩余票数：%ld 窗口：%@", ticketCount, [NSThread currentThread].name]);
+                [NSThread sleepForTimeInterval:0.2];
+            }
+            //如果已卖完，关闭售票窗口
+            else {
+                if ([NSThread currentThread].isCancelled)
+                {
+                    [NSThread exit];
+                    break;
+                }
+                else
+                {
+                    NSLog(@"售卖完毕");
+                    //给当前线程标记为取消状态
+                    [[NSThread currentThread] cancel];
+                    //停止当前线程的runLoop
+                    CFRunLoopStop(CFRunLoopGetCurrent());
+                }
+            }
+        }
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
